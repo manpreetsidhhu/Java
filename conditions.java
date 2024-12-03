ea8f54d57dda71b5f6a9fda285a53c780ed70e58
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-12-03 23:08:56 +0530

    Scanner Sc input

--- conditions.java
+++ conditions.java
@@ -3,26 +3,21 @@ import java.util.*;
 
 public class conditions {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Calculator:");
-        System.out.println("Enter 1st number:");
-        int a = sc.nextInt();
-        System.out.println("Enter 2nd number:");
-        int b = sc.nextInt();
-        System.out.println("Enter the operation:");
-        Scanner sc1 = new Scanner(System.in);
-        String op = sc1.nextLine();
-        if (op.equals("+")) {
-            System.out.println("The sum is: " + (a + b));
-        } else if (op.equals("-")) {
-            System.out.println("The difference is: " + (a - b));
-        } else if (op.equals("*")) {
-            System.out.println("The product is: " + (a * b));
-        } else if (op.equals("/")) {
-            System.out.println("The quotient is: " + (a / b));
-        } else {
-            System.out.println("Invalid operation");
+        try (Scanner sc = new Scanner(System.in); Scanner sc1 = new Scanner(System.in)) {
+            System.out.println("Calculator:");
+            System.out.println("Enter 1st number:");
+            int a = sc.nextInt();
+            System.out.println("Enter 2nd number:");
+            int b = sc.nextInt();
+            System.out.println("Enter the operation:");
+            String op = sc1.nextLine();
+            switch (op) {
+                case "+" -> System.out.println("The sum is: " + (a + b));
+                case "-" -> System.out.println("The difference is: " + (a - b));
+                case "*" -> System.out.println("The product is: " + (a * b));
+                case "/" -> System.out.println("The quotient is: " + (a / b));
+                default -> System.out.println("Invalid operation");
+            }
         }
-        sc.close(); sc1.close();
     }
 }
